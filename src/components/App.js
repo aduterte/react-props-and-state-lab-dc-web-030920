5cd22ffa01dd8853e122c3cf62472757e0a186ce
aduterte <alexduterte@gmail.com>
2020-05-12 18:17:13 -0400

    javascript is awful

--- src/components/App.js
+++ src/components/App.js
@@ -13,21 +13,69 @@ class App extends React.Component {
         type: 'all'
       }
     }
+    // this.onAdoptPet = this.onAdoptPet.bind(this)
   }
 
+  onAdoptPet = (event) => {
+    
+    let id = event.target.id
+    let newPetArr = this.state.pets.map(pet => {
+      // debugger
+      if (pet.id === id) {
+        pet.isAdopted = true
+      }
+     return pet
+    })
+    const t = this
+    this.setState({
+       pets: newPetArr
+    })
+    // debugger
+    // pet.isAdopted = true
+   
+  }
+  
+
+ 
+  
+  filter = (event) => {
+    // debugger
+    this.setState({
+          filters: {type: event.target.value}
+            
+            })
+   
+  }
+
+  onFindPetsClick = () => {
+    // debugger
+    let type = this.state.filters.type
+    
+    fetch(`/api/pets?type=${type}`)
+    .then(resp => resp.json())
+    .then(data => this.setState({pets: data}))
+    
+  }
+
+ 
+
   render() {
+   
+ 
+    // debugger
     return (
       <div className="ui container">
+       
         <header>
           <h1 className="ui dividing header">React Animal Shelter</h1>
         </header>
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters func={this.filter} findPet={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} func={this.onAdoptPet} />
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,38 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+  // constructor(){
+  //   super(),
+  //   this.state = {
+
+  //   }
+  // }
+  // onAdoptPet = (event) => {
+
+  //   let id = event.target.id
+  //   let pet = this.props.pets.find(obj => obj.id === id)
+  //   debugger
+  //   pet.isAdopted = true
+   
+  // }
+  
+  
+  
+  
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    let arr = this.props.pets
+    // arr.map( pet => {
+    //   <Pet petObj={pet}/>
+    // })
+
+    
+
+    console.log("the type is: ", this.props.pets)
+    return (
+      <div className="ui cards" > 
+        {arr.map(pet => <Pet key={pet.id} pet={pet} adopt={this.props.func} />)}
+      </div>
+    )
   }
 }
 
